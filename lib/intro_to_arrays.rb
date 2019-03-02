0d6425f167fa853379e201ec361e1439b2f6b55e
Knarquelya O'Neal <knarquelya.oneal@gmail.com>
2019-03-02 15:35:25 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,22 @@
+def instantiate_new_array
+  instantiate_new_array =  ["Array"]
+end
+
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  array_with_two_elements = ["array", "world"]
+end
+
+def first_element(arg)
+  first_element(0)
+end
+
+
+
+  
+
+
+  
